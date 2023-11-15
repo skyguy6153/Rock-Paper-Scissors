cd1034e7e11e99e6ddae34725a2c04fd7b69d99b
Aaron Green <53711550+skyguy6153@users.noreply.github.com>
2023-11-14 22:27:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const computerChoice = document.getElementById('computer-choice')
+const playerChoice = document.getElementById('player-choice')
+const resulDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', () => {
+    
+}))
